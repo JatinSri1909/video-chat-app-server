2bebdef24061f4f091881f09c2674c726fdc4058
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-09-28 15:01:34 +0530

    Initialized index.js

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const { Server } = require ('socket.io');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
 
-const app = express ();
-const io = new Server (app);
+const app = express();
+const io = new Server();
 
-app.use (bodyParser.json ());
+PORT = process.env.PORT || 5000;
+SOCKET_PORT = process.env.SOCKET_PORT || 8080;
 
+app.use(bodyParser.json());
 
-io.on('connection' ,() => {
-    console.log('a user connected');
-})
+const emailToSocketMapping = new Map();
 
-io.listen(8080, () => {
-    console.log('Server is running on port 8080');
+io.on("connection", (socket) => {
+  socket.on("join-room", (data) => {
+    const { roomId, emailId } = data;
+    console.log("User", emailId, "joined room", roomId);
+    emailToSocketMapping.set(emailId, socket.id);
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", emailId);
+  });
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-})
+io.listen(SOCKET_PORT, () => {
+  console.log(`Server is running on port ${SOCKET_PORT}`);
+});
 
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
