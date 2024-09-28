d0b143032f0c122479efaddebcd1473187b1f653
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-09-28 16:55:47 +0530

    created logic for getting offer.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ SOCKET_PORT = process.env.SOCKET_PORT || 8080;
 app.use(bodyParser.json());
 
 const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
 
 io.on("connection", (socket) => {
   console.log("User connected");
@@ -21,9 +22,17 @@ io.on("connection", (socket) => {
     const { roomId, emailId } = data;
     console.log("User", emailId, "joined room", roomId);
     emailToSocketMapping.set(emailId, socket.id);
+    socketToEmailMapping.set(socket.id, emailId);
     socket.join(roomId);
     socket.emit("joined-room", { roomId });
      socket.broadcast.to(roomId).emit("user-connected", emailId);
+  });
+
+  socket.on('call-user', (data) => {
+    const { emailId, offer } = data;
+    const fromEmail = socketToEmailMapping.get(socket.id);
+    const socketId = emailToSocketMapping.get(emailId);
+    socket.to(socketId).emit('incoming-call', { from: fromEmail, offer });
   });
 });
 
