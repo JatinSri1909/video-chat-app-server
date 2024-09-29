3fc406598ff8fdc6340b0e63ef0ffce23555ea39
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-09-29 19:22:44 +0530

    Refactor socket event names and add logging for call actions

--- index.js
+++ index.js
@@ -25,21 +25,37 @@ io.on("connection", (socket) => {
     socketToEmailMapping.set(socket.id, emailId);
     socket.join(roomId);
     socket.emit("joined-room", { roomId });
-     socket.broadcast.to(roomId).emit("user-connected", {emailId});
+    socket.broadcast.to(roomId).emit("user-joined", { emailId });
   });
 
-  socket.on('call-user', (data) => {
+  socket.on("call-user", (data) => {
     const { emailId, offer } = data;
     const fromEmail = socketToEmailMapping.get(socket.id);
     const socketId = emailToSocketMapping.get(emailId);
-    socket.to(socketId).emit('incoming-call', { from: fromEmail, offer });
+    console.log(
+      "Calling user",
+      emailId,
+      "from",
+      fromEmail,
+      "with offer",
+      offer
+    );
+    socket.to(socketId).emit("incoming-call", { from: fromEmail, offer });
   });
 
-  socket.on('call-accepted', (data) => {
+  socket.on("call-accepted", (data) => {
     const { emailId, ans } = data;
     const fromEmail = socketToEmailMapping.get(socket.id);
     const socketId = emailToSocketMapping.get(emailId);
-    socket.to(socketId).emit('call-accepted', { from: fromEmail, ans });
+    console.log(
+      "Call accepted by",
+      emailId,
+      "from",
+      fromEmail,
+      "with answer",
+      ans
+    );
+    socket.to(socketId).emit("call-accepted", { from: fromEmail, ans });
   });
 });
 
